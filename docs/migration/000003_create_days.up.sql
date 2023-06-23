312cf7a7079a1a0ec4ca089c02c00e68a78d39cc
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-06-24 00:05:21 +0900

    daysテーブルを追加

---
+++ docs/migration/000003_create_days.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `days` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `day_key` VARCHAR(12) NOT NULL UNIQUE,
+    `spot_id` INT NOT NULL,
+    `count` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE days
+ADD CONSTRAINT fk_days_spots
+FOREIGN KEY (spot_id)
+REFERENCES spots(id);
