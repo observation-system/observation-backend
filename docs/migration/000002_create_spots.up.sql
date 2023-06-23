2de332481fd8cfdb69a2c8efcb6a4425afb50ef5
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-06-24 00:05:09 +0900

    spotsテーブルを追加

---
+++ docs/migration/000002_create_spots.up.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `spots` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `spot_key` VARCHAR(12) NOT NULL UNIQUE,
+    `user_id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `url` VARCHAR(255) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `latitude` VARCHAR(255) NOT NULL,
+    `longitude` VARCHAR(255) NOT NULL,
+    `count` VARCHAR(255) NOT NULL,
+    `count_day` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE spots
+ADD CONSTRAINT fk_spots_users
+FOREIGN KEY (user_id)
+REFERENCES users(id);
