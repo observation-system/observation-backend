f5aa17c62140ca8ec2b967dae3db16acd3c14ed3
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-06-25 19:16:28 +0900

    バッチログの処理を追加

--- log/log.go
+++ log/log.go
@@ -12,3 +12,12 @@ func GeneratApiLog() (fp *os.File) {
 
 	return fp
 }
+
+func GeneratBatchLog() (fp *os.File) {
+	fp, err := os.OpenFile("log/batch_debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	return fp
+}
